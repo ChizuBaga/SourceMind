58c6790d538a086b11d14656f6adcb86e31657f7
Dennis <dddummy499@gmail.com>
2025-07-20 17:44:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain_huggingface import HuggingFaceEmbeddings
 
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoModelForCausalLM
 
 import PyPDF2
 import argparse
